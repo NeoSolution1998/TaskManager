7ac85f6200c79ce6fe65f7e21d19f077239cabfa
neosolution <neosolution1998@gmail.com>
2022-07-03 08:22:48 +0300

    fix home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container py-4">
-        <div class="jumbotron">
-            <h1 class="display-4">@lang("Привет от Хекслета!")</h1>
-            <p class="lead">Практические курсы по программированию</p>
-            <hr class="my-4">
-            <a class="btn btn-primary btn-lg" href="https://hexlet.io" role="button">Узнать больше</a>
+
+    <body>
+        <div class="container py-4">
+            <div class="jumbotron">
+                <h1 class="display-4">@lang('Привет от Хекслета!')</h1>
+                <p class="lead">Практические курсы по программированию</p>
+                <hr class="my-4">
+                <a class="btn btn-primary btn-lg" href="https://hexlet.io" role="button">Узнать больше</a>
+            </div>
         </div>
-    </div>
+    </body>
 @endsection
