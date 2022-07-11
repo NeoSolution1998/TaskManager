a06f2f815bf6781b4b9e72a16045907b0b03d50d
neosolution <neosolution1998@gmail.com>
2022-07-12 00:01:51 +0300

    fix linter

--- app/Models/Label.php
+++ app/Models/Label.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * App\Models\Label
+ *
+ * @property string $name
+ * @property string $description
+ */
 class Label extends Model
 {
     use HasFactory;
@@ -16,6 +22,11 @@ class Label extends Model
         'description'
     ];
 
+    /**
+     * Return collection of tasks associated with given label
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function tasks()
     {
         return $this->belongsToMany(Task::class);
