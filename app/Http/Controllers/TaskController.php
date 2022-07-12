3332249c43f786050fc060ffd5ec0bdafe775d95
neosolution <neosolution1998@gmail.com>
2022-07-12 20:35:22 +0300

    правки 1.7

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Auth;
 use Spatie\QueryBuilder\QueryBuilder;
 use Spatie\QueryBuilder\AllowedFilter;
 use App\Http\Requests\StoreTaskRequest;
+use Exception;
 
 class TaskController extends Controller
 {
